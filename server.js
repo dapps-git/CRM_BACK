2c95addc82ae1f64e9b3e258f476113a3786a457
Your Name <aifasanaauk@gmail.com>
2026-07-24 20:10:12 +0530

    Explicitly include lowercase authorization in LiteSpeed .htaccess and Express CORS headers

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ connectDB().then(() => {
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
-  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'authorization, Authorization, Content-Type, Accept, Origin, X-Requested-With, X-CSRF-Token, *');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
 
   if (req.method === 'OPTIONS') {
