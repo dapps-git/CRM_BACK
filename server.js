e2106d809db2f1c21083f51c44a3d768c9304541
Your Name <aifasanaauk@gmail.com>
2026-07-24 20:09:31 +0530

    Allow wildcard Access-Control-Allow-Headers at LiteSpeed web server level and Express

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ connectDB().then(() => {
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, authorization, *');
+  res.setHeader('Access-Control-Allow-Headers', '*');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
 
   if (req.method === 'OPTIONS') {
